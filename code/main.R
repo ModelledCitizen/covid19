199cf22f5a72ecd8734e4d06589b11cb7cafe20d
UnlikelyVolcano <50756646+UnlikelyVolcano@users.noreply.github.com>
2020-04-11 19:12:21 -0400

    Automatic update

--- code/main.R
+++ code/main.R
@@ -10,18 +10,29 @@ worldometers <-
   read_html("https://www.worldometers.info/coronavirus/") %>%
   html_nodes("#main_table_countries_today") %>%
   html_table() %>%
-  (function(x) {x[[1]]})
+  (function(x) {
+    x[[1]]
+  })
 
 
 # The COVID Tracking Project ----------------------------------------------
 
-ctp_us_live <- read.csv("https://covidtracking.com/api/us.csv", stringsAsFactors = F)
-ctp_us_time <- read.csv("https://covidtracking.com/api/us/daily.csv", stringsAsFactors = F)
-ctp_st_live <- read.csv("https://covidtracking.com/api/states.csv", stringsAsFactors = F)
-ctp_st_time <- read.csv("https://covidtracking.com/api/states/daily.csv", stringsAsFactors = F)
+ctp_us_live <-
+  read.csv("https://covidtracking.com/api/us.csv", stringsAsFactors = F)
+ctp_us_time <-
+  read.csv("https://covidtracking.com/api/us/daily.csv",
+           stringsAsFactors = F)
+ctp_st_live <-
+  read.csv("https://covidtracking.com/api/states.csv",
+           stringsAsFactors = F)
+ctp_st_time <-
+  read.csv("https://covidtracking.com/api/states/daily.csv",
+           stringsAsFactors = F)
 
-ctp_us_time$date <- as.Date(strptime(ctp_us_time$date, format = "%Y%m%d"))
-ctp_st_time$date <- as.Date(strptime(ctp_st_time$date, format = "%Y%m%d"))
+ctp_us_time$date <-
+  as.Date(strptime(ctp_us_time$date, format = "%Y%m%d"))
+ctp_st_time$date <-
+  as.Date(strptime(ctp_st_time$date, format = "%Y%m%d"))
 
 ctp_st_live$fips <- sprintf("%02d", ctp_st_live$fips)
 ctp_st_time$fips <- sprintf("%02d", ctp_st_time$fips)
@@ -36,8 +47,10 @@ nyt_counties <-
   )
 nyt_counties$fips <- sprintf("%05d", nyt_counties$fips)
 nyt_counties$date <- as.Date(nyt_counties$date)
-nyt_update_date <- sort(unique(nyt_counties$date), decreasing = T)[1]
-nyt_counties_live <- nyt_counties[nyt_counties$date == nyt_update_date,]
+nyt_update_date <-
+  sort(unique(nyt_counties$date), decreasing = T)[1]
+nyt_counties_live <-
+  nyt_counties[nyt_counties$date == nyt_update_date, ]
 
 nyt_states <-
   read.csv(
@@ -46,58 +59,86 @@ nyt_states <-
   )
 nyt_states$fips <- sprintf("%05d", nyt_states$fips)
 nyt_states$date <- as.Date(nyt_states$date)
-nyt_states_live <- nyt_states[nyt_states$date == nyt_update_date,]
+nyt_states_live <- nyt_states[nyt_states$date == nyt_update_date, ]
 
 nyt_update_date <- format.Date(nyt_update_date, "%d %B %Y")
 
 
 # Johns Hopkins University ------------------------------------------------
 
-jhu_url <- "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/"
+jhu_url <-
+  "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/"
 jhu_ts_confi <-
   read.csv(
-    paste0(jhu_url, "csse_covid_19_time_series/time_series_covid19_confirmed_global.csv"),
+    paste0(
+      jhu_url,
+      "csse_covid_19_time_series/time_series_covid19_confirmed_global.csv"
+    ),
     stringsAsFactors = F
   )
 jhu_ts_death <-
   read.csv(
-    paste0(jhu_url, "csse_covid_19_time_series/time_series_covid19_deaths_global.csv"),
+    paste0(
+      jhu_url,
+      "csse_covid_19_time_series/time_series_covid19_deaths_global.csv"
+    ),
     stringsAsFactors = F
   )
-jhu_daily_base <- paste0(jhu_url, "csse_covid_19_daily_reports/%s.csv")
+jhu_daily_base <-
+  paste0(jhu_url, "csse_covid_19_daily_reports/%s.csv")
 tryCatch({
   jhu_update_date <- format.Date(Sys.Date(), "%d %B %Y")
-  jhu_daily_url <- sprintf(jhu_daily_base, format.Date(Sys.Date(), "%m-%d-%Y"))
+  jhu_daily_url <-
+    sprintf(jhu_daily_base, format.Date(Sys.Date(), "%m-%d-%Y"))
   jhu_daily_update <- read.csv(jhu_daily_url, stringsAsFactors = F)
 },
 error = function(cond) {
   jhu_update_date <<- format.Date(Sys.Date() - 1, "%d %B %Y")
-  jhu_daily_url <<- sprintf(jhu_daily_base, format.Date(Sys.Date() - 1, "%m-%d-%Y"))
+  jhu_daily_url <<-
+    sprintf(jhu_daily_base, format.Date(Sys.Date() - 1, "%m-%d-%Y"))
   jhu_daily_update <<- read.csv(jhu_daily_url, stringsAsFactors = F)
-}, finally = {})
+}, finally = {
+})
 
 
 # PA Department of Public Health ------------------------------------------
 
 pa.counties <-
   read_html("https://www.health.pa.gov/topics/disease/coronavirus/Pages/Cases.aspx") %>%
-  html_nodes("#ctl00_PlaceHolderMain_PageContent__ControlWrapper_RichHtmlField > div > div > table") %>%
+  html_nodes(
+    "#ctl00_PlaceHolderMain_PageContent__ControlWrapper_RichHtmlField > div > div > table"
+  ) %>%
   html_table() %>%
-  (function(x) {x[[1]]}) %>%
-  (function(x) {colnames(x) <- x[1,]; x[-1,]})
+  (function(x) {
+    x[[1]]
+  }) %>%
+  (function(x) {
+    colnames(x) <- x[1, ]
+    x[-1, ]
+  })
 
 pa.overall <-
   read_html("https://www.health.pa.gov/topics/disease/coronavirus/Pages/Cases.aspx") %>%
-  html_nodes("#ctl00_PlaceHolderMain_PageContent__ControlWrapper_RichHtmlField > table:nth-child(2)") %>%
+  html_nodes(
+    "#ctl00_PlaceHolderMain_PageContent__ControlWrapper_RichHtmlField > table:nth-child(2)"
+  ) %>%
   html_table() %>%
-  (function(x) {x[[1]]}) %>%
-  (function(x) {colnames(x) <- x[1,]; x[-1,]})
+  (function(x) {
+    x[[1]]
+  }) %>%
+  (function(x) {
+    colnames(x) <- x[1, ]
+    x[-1, ]
+  })
 
 pa.update <-
   read_html("https://www.health.pa.gov/topics/disease/coronavirus/Pages/Cases.aspx") %>%
-  html_nodes("#ctl00_PlaceHolderMain_PageContent__ControlWrapper_RichHtmlField > p:nth-child(3) > span") %>%
+  html_nodes(
+    "#ctl00_PlaceHolderMain_PageContent__ControlWrapper_RichHtmlField > p:nth-child(3) > span"
+  ) %>%
   html_text() %>%
-  (function(x) gsub("\\.", "", x)) %>%
+  (function(x)
+    gsub("\\.", "", x)) %>%
   strptime("* Map, tables and case counts last updated at %I:%M %p on %m/%e/%Y") %>%
   format.Date("%d %B %Y at %H:%M")
 
@@ -105,7 +146,7 @@ pa.update <-
 # Philadelphia Department of Public Health --------------------------------
 
 ph_url <-
-  "https://public.tableau.com/vizql/w/COP-COVID-19/v/Stacked/vud/sessions/0817917F14C44B11BAA76A6354F9B410-0:0/views/7713620505763405234_17874758422068147630?csv=true&showall=true"
+  "https://public.tableau.com/vizql/w/COP-COVID-19/v/Stacked/vud/sessions/EA8E5103C4F3441FAFD0291C6F66D37F-0:0/views/7713620505763405234_17874758422068147630?csv=true&showall=true"
 tryCatch({
   cop <- read.csv(ph_url, stringsAsFactors = F)
 },
@@ -133,7 +174,7 @@ for (i in 1:nrow(cop2)) {
   cop3$POS[i] <- sum(cop2$POS[1:i])
 }
 cop3[["TOT"]] <- cop3$NEG + cop3$POS
-cop3 <- cop3[cop3$Date > "2020-02-29", ]
+cop3 <- cop3[cop3$Date > "2020-02-29",]
 
 phl.update <-
   format(strptime(unique(cop$UpdatedDate), format = "%m/%e/%Y %I:%M:%S %p"),
@@ -148,7 +189,8 @@ jhu_us_ts <-
     cases = colSums(jhu_ts_confi[jhu_ts_confi$Country.Region %in% "US" , -(1:4)], na.rm = T),
     deaths = colSums(jhu_ts_death[jhu_ts_death$Country.Region %in% "US" , -(1:4)], na.rm = T)
   )
-jhu_us_live <- jhu_daily_update[jhu_daily_update$Country_Region %in% "US",]
+jhu_us_live <-
+  jhu_daily_update[jhu_daily_update$Country_Region %in% "US", ]
 
 ctp_st_time <-
   merge(
@@ -159,11 +201,15 @@ ctp_st_time <-
     all.x = T
   )
 
-us_new_cases <- jhu_us_ts$cases[-1] - jhu_us_ts$cases[-nrow(jhu_us_ts)]
-us_new_deaths <- jhu_us_ts$deaths[-1] - jhu_us_ts$deaths[-nrow(jhu_us_ts)]
+us_new_cases <-
+  jhu_us_ts$cases[-1] - jhu_us_ts$cases[-nrow(jhu_us_ts)]
+us_new_deaths <-
+  jhu_us_ts$deaths[-1] - jhu_us_ts$deaths[-nrow(jhu_us_ts)]
 us_by_day <- jhu_us_ts$cases[-1] / jhu_us_ts$cases[-nrow(jhu_us_ts)]
-us_by_day_d <- jhu_us_ts$deaths[-1] / jhu_us_ts$deaths[-nrow(jhu_us_ts)]
-us_growth_rate <- us_new_cases[-1] / us_new_cases[-length(us_new_cases)]
+us_by_day_d <-
+  jhu_us_ts$deaths[-1] / jhu_us_ts$deaths[-nrow(jhu_us_ts)]
+us_growth_rate <-
+  us_new_cases[-1] / us_new_cases[-length(us_new_cases)]
 us_growth_data <- data.frame(
   date = jhu_us_ts$date[-(1:2)],
   cases = jhu_us_ts$cases[-(1:2)],
@@ -174,8 +220,13 @@ us_growth_data <- data.frame(
   new_deaths = us_new_deaths[-1],
   change_new = us_growth_rate
 )
-rm(us_new_cases, us_new_deaths, us_by_day, us_by_day_d, us_growth_rate)
-us_growth_data$change_new[is.infinite(us_growth_data$change_new)] <- 0
+rm(us_new_cases,
+   us_new_deaths,
+   us_by_day,
+   us_by_day_d,
+   us_growth_rate)
+us_growth_data$change_new[is.infinite(us_growth_data$change_new)] <-
+  0
 us_growth_data$change_new[is.nan(us_growth_data$change_new)] <- 0
 
 
@@ -196,7 +247,8 @@ wrld_by_day <-
   world_ts$cases[-1] / world_ts$cases[-nrow(world_ts)]
 wrld_by_day_d <-
   world_ts$deaths[-1] / world_ts$deaths[-nrow(world_ts)]
-wrld_growth_rate <- wrld_new_cases[-1] / wrld_new_cases[-length(wrld_new_cases)]
+wrld_growth_rate <-
+  wrld_new_cases[-1] / wrld_new_cases[-length(wrld_new_cases)]
 wrld_growth_data <- data.frame(
   date = world_ts$date[-(1:2)],
   cases = world_ts$cases[-(1:2)],
@@ -207,8 +259,13 @@ wrld_growth_data <- data.frame(
   new_deaths = wrld_new_deaths[-1],
   change_new = wrld_growth_rate
 )
-rm(wrld_new_cases, wrld_new_deaths, wrld_by_day, wrld_by_day_d, wrld_growth_rate)
-wrld_growth_data$change_new[is.infinite(us_growth_data$change_new)] <- 0
+rm(wrld_new_cases,
+   wrld_new_deaths,
+   wrld_by_day,
+   wrld_by_day_d,
+   wrld_growth_rate)
+wrld_growth_data$change_new[is.infinite(us_growth_data$change_new)] <-
+  0
 wrld_growth_data$change_new[is.nan(us_growth_data$change_new)] <- 0
 
 
