85cef294bdd08705895f15effcd9d246eba0ff19
UnlikelyVolcano <50756646+UnlikelyVolcano@users.noreply.github.com>
2020-04-14 15:24:33 -0400

    Automatic update

--- code/sandbox.R
+++ code/sandbox.R
@@ -19,7 +19,13 @@ pa.counties <-
   (function(x) {colnames(x) <- x[1,]; x[-1,]})
 
 
-
-
 "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/UID_ISO_FIPS_LookUp_Table.csv"
 
+
+library(RCurl)
+library(jsonlite)
+jsn <-
+  getURL(
+    "https://covid19-static.cdn-apple.com/covid19-mobility-data/2005HotfixDev12/v1/en-us/applemobilitytrends.json"
+  ) %>% fromJSON()
+str(jsn$data$Philadelphia)
