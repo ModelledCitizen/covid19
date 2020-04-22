ca115afcfbefa0b7af5b153a494b0db0338c5473
UnlikelyRobot <robot@unlikelyvolcano.com>
2020-04-22 18:19:21 +0000

    Automatic update

--- code/autoupdate.R
+++ code/autoupdate.R
@@ -4,74 +4,78 @@ rm(list = ls())
 
 sink("logfile.txt")
 
-while (TRUE) {
 
-  if (as.numeric(format(Sys.time(), "%M")) == 0)  {
-
-    cat(format(Sys.time()))
-    cat(": Starting update...\n")
-
-    cat(format(Sys.time()))
-    cat(": Pull repo...\n")
-    system("git fetch; git pull")
-
-    # Get Data ----------------------------------------------------------------
-
-    cat(format(Sys.time()))
-    cat(": Get data...\n")
-    source("code/get_data.R")
-
-
-    # Knit Report -------------------------------------------------------------
-
-
-    cat(format(Sys.time()))
-    cat(": Render index...\n")
-    rmarkdown::render("report.Rmd", output_file = "index.html", quiet = T)
-
-
-    if (as.numeric(format(Sys.time(), "%H")) %%  2 == 0)  {
-      # Update World Map --------------------------------------------------------
+tryCatch({
+  while (TRUE) {
+    if (as.numeric(format(Sys.time(), "%M")) == 0)  {
+      cat(format(Sys.time()))
+      cat(": Starting update...\n")
 
       cat(format(Sys.time()))
-      cat(": Save world map...\n")
-      source("code/world-map.R")
+      cat(": Pull repo...\n")
+      system("git fetch; git pull")
 
-
-      # Update US Map -----------------------------------------------------------
-
-      cat(format(Sys.time()))
-      cat(": Save US map...\n")
-      source("code/us-map.R")
-
-
-      # Update US Map GIF -------------------------------------------------------
+      # Get Data ----------------------------------------------------------------
 
       cat(format(Sys.time()))
-      cat(": Save spread GIF...\n")
-      source("code/spread.R")
+      cat(": Get data...\n")
+      source("code/get_data.R")
+
+      # Knit Report -------------------------------------------------------------
+
+      cat(format(Sys.time()))
+      cat(": Render index...\n")
+      rmarkdown::render("report.Rmd", output_file = "index.html", quiet = T)
+
+      if (as.numeric(format(Sys.time(), "%H")) %% 4 == 0)  {
+        # Update World Map --------------------------------------------------------
+
+        cat(format(Sys.time()))
+        cat(": Save world map...\n")
+        source("code/world-map.R")
+
+        # Update US Map -----------------------------------------------------------
+
+        cat(format(Sys.time()))
+        cat(": Save US map...\n")
+        source("code/us-map.R")
+
+        # Update US Map GIF -------------------------------------------------------
+
+        cat(format(Sys.time()))
+        cat(": Save spread GIF...\n")
+        source("code/spread.R")
+
+      }
+
+      cat(format(Sys.time()))
+      cat(": Commit changes...\n")
+      system("git add .; git commit -m 'Automatic update'")
+
+      cat(format(Sys.time()))
+      cat(": Push repo...\n")
+      system("git push")
+
+      cat(format(Sys.time()))
+      cat(": Send alert...\n")
+      source("code/alert.R")
+
+      cat(format(Sys.time()))
+      cat(": Sleep for one minute...\n")
+      Sys.sleep(60)
+
+      cat(format(Sys.time()))
+      cat(": Wake up...\n")
 
     }
-
-    cat(format(Sys.time()))
-    cat(": Commit changes...\n")
-    system("git add .; git commit -m 'Automatic update'")
-
-    cat(format(Sys.time()))
-    cat(": Push repo...\n")
-    system("git push")
-
-    cat(format(Sys.time()))
-    cat(": Send alert...\n")
-    source("code/alert.R")
-
-    cat(format(Sys.time()))
-    cat(": Sleep for one minute...\n")
-    Sys.sleep(60)
-
-    cat(format(Sys.time()))
-    cat(": Wake up...\n")
-
   }
-
-}
+},
+error = function(cond) {
+  cat(format(Sys.time()))
+  cat(": Sending Error message..\n")
+  source("code/error.R")
+},
+finally = {
+  cat(format(Sys.time()))
+  cat(": This code should never run..\n")
+})
