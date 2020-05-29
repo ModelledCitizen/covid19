82c4a321f0ede78acfa11fae733fe3dc7a1c0a08
UnlikelyRobot <robot@unlikelyvolcano.com>
2020-05-28 23:26:15 -0400

    Automatic update

--- code/get_data.R
+++ code/get_data.R
@@ -118,7 +118,7 @@ error = function(cond) {
 pa.counties <-
   read_html("https://www.health.pa.gov/topics/disease/coronavirus/Pages/Cases.aspx") %>%
   html_nodes(
-    "#ctl00_PlaceHolderMain_PageContent__ControlWrapper_RichHtmlField > div > div > table"
+    "#ctl00_PlaceHolderMain_PageContent__ControlWrapper_RichHtmlField > table:nth-child(25)"
   ) %>%
   html_table() %>%
   (function(x) {
